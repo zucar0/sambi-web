86b3330a5b5078137d959333576e7931f9859f7d
Antonio Enríquez <antonio.enralv@gmail.com>
2021-12-10 15:49:34 -0600

    prototipo funcional en 000webhost

--- contacto.php
+++ contacto.php
@@ -1,58 +1,67 @@
 <?php
- 
-if($_POST) {
-    $visitor_name = "";
-    $visitor_email = "";
-    $email_title = "";
-    $concerned_department = "";
-    $visitor_message = "";
-     
-    if(isset($_POST['visitor_name'])) {
-      $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['visitor_email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
-    }
-     
-    if(isset($_POST['email_title'])) {
-        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['concerned_department'])) {
-        $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['visitor_message'])) {
-        $visitor_message = htmlspecialchars($_POST['visitor_message']);
-    }
-     
-    if($concerned_department == "ventas") {
-        $recipient = "ventas@tecplay.com";
-    }
-    else if($concerned_department == "capacitacion") {
-        $recipient = "capacitacion@tecplay.com";
-    }
-    else if($concerned_department == "sugerencias") {
-        $recipient = "sugerencias@tecplay.com";
-    }
-    else {
-        $recipient = "contacto@tecplay.com";
-    }
-     
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $visitor_email . "\r\n";
-     
-    if(mail($recipient, $email_title, $visitor_message, $headers)) {
-        echo "<p>Gracias por contactarnos, $visitor_name. Te responderemos lo más pronto posible.</p>";
-    } else {
-        echo '<p>Lo sentimos hay un problema con el email.</p>';
-    }
-     
-} else {
-    echo '<p>Ocurrió un error</p>';
+if(isset($_POST['email'])) {
+$email_to = "antonio.enralv@gmail.com";
+$email_subject = "Propuesta resumida del correo electrónico";
+//Se muestra un error si existe un problema en los campos del formulario. 
+function died($error) {
+    echo "Lo sentimos no pudimos procesar tu solicitud. ";
+    echo "A continuación te mostramos un listado con los posibles errores: <br /><br />";
+    echo $error."<br /><br />";
+    echo "Por favor intenta nuevamente.<br /><br />";
+    die();
 }
- 
+// Validación de los datos existentes 
+if(!isset($_POST['first_name']) ||
+       !isset($_POST['last_name']) ||
+       !isset($_POST['email']) ||
+       !isset($_POST['telephone']) ||
+       !isset($_POST['comments'])) {
+    died('Lo sentimos; se identificó un error con las siguientes entradas');   
+}
+$first_name = $_POST['first_name']; // requerido
+$last_name = $_POST['last_name']; // requerido
+$email_from = $_POST['email']; // requerido
+   $telephone = $_POST['telephone']; // no requerido 
+$comments = $_POST['comments']; // requerido
+$error_message = "";
+$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ if(!preg_match($email_exp,$email_from)) {
+$error_message .= 'Ingresaste un email inválido<br />';
+ }
+$string_exp = "/^[A-Za-z .'-]+$/";
+ if(!preg_match($string_exp,$first_name)) {
+$error_message .= 'El nombre no es válido.<br />';
+ }
+ if(!preg_match($string_exp,$last_name)) {
+$error_message .= 'El apellido no es válido.<br />';
+ }
+ if(strlen($comments) < 2) {
+$error_message .= 'El comentario no es válido.<br />';
+ }
+ if(strlen($error_message) > 0) {
+   died($error_message);
+ }
+$email_message = "Esta es la información ingresada por el usuario:\n\n";
+function clean_string($string) {
+  $bad = array("content-type","bcc:","to:","cc:","href");
+  return str_replace($bad,"",$string);
+}
+$email_message .= "Nombre: ".clean_string($first_name)."\n";
+$email_message .= "Apellido: ".clean_string($last_name)."\n";
+$email_message .= "Email: ".clean_string($email_from)."\n";
+$email_message .= "Teléfono: ".clean_string($telephone)."\n";
+$email_message .= "Comentario: ".clean_string($comments)."\n";
+// Se crean las cabeceras del correo
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+?>
+<!-- Mensaje HTML -->
+echo'<script type="text/javascript">
+        alert("Gracias por contactarnos. Te contactaremos lo más pronto posible.");
+        window.location.href="contacto.html";
+        </script>';
+<?php
+}
 ?>
